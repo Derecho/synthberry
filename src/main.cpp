e016f7e39297fdcf6216b8395f2477f80475e27a
Derecho <derecho@sector5d.org>
2014-02-22 22:45:19 +0100

    + src/main: Implemented a basic main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "serialmidi.h"
 
 int main(int argc, char *argv[])
 {
+    std::cout << "Synthberry starting..." << std::endl;
+
+    IMidiSource *midi = new SerialMidi("/dev/ttyAMA0");
+    midi->run();
+
     return 0;
 }
