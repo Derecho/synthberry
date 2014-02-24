a81bfbfe3019bd4e4849a6648c2c483192ab8a89
Derecho <derecho@sector5d.org>
2014-02-24 23:32:58 +0100

    * src/notes: Presence check in addNote() should break when done. Renamed lambda in removeNote().

--- src/notes.cpp
+++ src/notes.cpp
@@ -12,9 +12,12 @@ Notes::~Notes()
 void Notes::addNote(uint8_t pitch, uint8_t velocity)
 {
     bool present = false;
-    for(auto &note : notes)
-        if(note.getPitch() == pitch)
+    for(auto &note : notes) {
+        if(note.getPitch() == pitch) {
             present = true;
+            break;
+        }
+    }
 
     if(!present) {
         Note note(pitch, velocity);
@@ -30,7 +33,7 @@ void Notes::removeNote(uint8_t pitch)
     // As we're providing a lambda to filter out which Note objects should be
     // removed, we can do the observer notification here as well to avoid
     // looping over the notes twice.
-    auto noteRemoveHelper = [this, pitch](Note note) {
+    auto notifyingPredicate = [this, pitch](Note note) {
         if(note.getPitch() == pitch) {
             for(auto &observer : this->observers)
                 observer->noteRemoved(note);
@@ -40,7 +43,7 @@ void Notes::removeNote(uint8_t pitch)
         return false;
     };
 
-    notes.erase(std::remove_if(notes.begin(), notes.end(), noteRemoveHelper),
+    notes.erase(std::remove_if(notes.begin(), notes.end(), notifyingPredicate),
             notes.end());
 }
 
