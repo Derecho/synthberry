218cc8c671cb9e2eafd67e74238d1a33464d7dd6
Derecho <derecho@sector5d.org>
2014-03-09 12:33:02 +0100

    + src/basicwave: Implemented getNextSample().

--- src/basicwave.cpp
+++ src/basicwave.cpp
@@ -1,10 +1,19 @@
 #include "basicwave.h"
 
 template <class SampleType> BasicWave<SampleType>::BasicWave(uint16_t bitrate) :
-    Wave<SampleType>(bitrate)
+    Wave<SampleType>(bitrate),
+    position(0)
 {
 }
 
 template <class SampleType> BasicWave<SampleType>::~BasicWave()
 {
+}
+
+template <class SampleType> SampleType BasicWave<SampleType>::getNextSample()
+{
+    if(position == this->bufferSize)
+        position = 0;
+
+    return this->buffer[position++];
 }
--- src/basicwave.h
+++ src/basicwave.h
@@ -8,9 +8,11 @@ template <class SampleType> class BasicWave : public Wave<SampleType>
 public:
     BasicWave(uint16_t bitrate);
     ~BasicWave();
+    SampleType getNextSample();
     // TODO EnvelopedWave<SampleType> toEnvelopedWave();
 
 private:
+    std::size_t position;
 };
 
 #endif  // BASICWAVE_H
