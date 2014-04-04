dda5963522f7b2e37daa7c69101fb14a1bfd413d
Derecho <derecho@sector5d.org>
2014-04-04 10:15:23 +0200

    Updated documentation

--- search/all_73.js
+++ search/all_73.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['serialmidi',['SerialMidi',['../classSerialMidi.html',1,'']]],
+  ['serialmidi',['SerialMidi',['../classSerialMidi.html',1,'SerialMidi'],['../classSerialMidi.html#aec6fc75e28ea5c1b473b608e1594d2dd',1,'SerialMidi::SerialMidi()']]],
   ['setlowestfrequency',['setLowestFrequency',['../classWave.html#a0c9b706b00099bc1fa000c58fe304be8',1,'Wave']]],
   ['setmidisource',['setMidiSource',['../classSynthesizer.html#afece07221f12f450a45227797139cf5d',1,'Synthesizer']]],
   ['setnewformula',['setNewFormula',['../classWave.html#a5e48493b1d534a660245251f37e738bd',1,'Wave']]],
